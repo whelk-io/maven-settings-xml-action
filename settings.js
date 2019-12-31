5b4243aec1a413809dd6160eebd362b723cb736f
Zack Teater <zteater@gmail.com>
2019-12-31 07:59:49 -0500

    update

--- settings.js
+++ settings.js
@@ -1,28 +1,28 @@
 const core = require('@actions/core');
-//const path = require('path');
-//const fs = require('fs');
-//const XMLSerializer = require('xmldom').XMLSerializer;
-//const DOMParser = require('xmldom').DOMParser;
+const path = require('path');
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
+const XMLSerializer = require('xmldom').XMLSerializer;
 
 function getSettingsTemplate() {
     core.info("opening settings template");
-    //const templatePath = path.join(__dirname, 'template', 'settings.xml');
-    //const template = fs.readFileSync(templatePath).toString();
-    //return new DOMParser().parseFromString(template, 'text/xml');
-    return null;
+    const templatePath = path.join(__dirname, 'template', 'settings.xml');
+    const template = fs.readFileSync(templatePath).toString();
+    return new DOMParser().parseFromString(template, 'text/xml');
 }
 
-//function writeSettings(settingsPath, templateXml) {
-//    if (!fs.existsSync(path.dirname(settingsPath))) {
-//        core.info("creating .m2 directory");
-//        fs.mkdirSync(path.dirname(settingsPath));
-//    }
-//
-//    core.info("writing m2 to path: " + settingsPath)
-//    const settingStr = new XMLSerializer().serializeToString(templateXml);
-//    fs.writeFileSync(settingsPath, settingStr);
-//}
+function writeSettings(settingsPath, templateXml) {
+    if (!fs.existsSync(path.dirname(settingsPath))) {
+        core.info("creating ~/.m2 directory");
+        fs.mkdirSync(path.dirname(settingsPath));
+    }
+
+    core.info("writing settings.xml to path: " + settingsPath)
+    const settingStr = new XMLSerializer().serializeToString(templateXml);
+    fs.writeFileSync(settingsPath, settingStr);
+}
 
 module.exports = {
-    getSettingsTemplate
+    getSettingsTemplate,
+    writeSettings
 }
