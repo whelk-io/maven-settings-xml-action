3d443a7cc04638382960a15c34cd92926b176b6a
Zack Teater <zteater@gmail.com>
2019-12-30 23:34:20 -0500

    initial java build

---
+++ src/main/java/io/whelk/action/maven/settings/App.java
@@ -0,0 +1,13 @@
+package io.whelk.action.maven.settings;
+
+import java.util.Arrays;
+
+public class App {
+
+  public static void main(String... args) {
+
+    System.out.println(Arrays.toString(args)); // TODO
+
+  }
+
+}
